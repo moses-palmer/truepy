ecf94fc3631899af34d1c0052757dc2143fd7574
Moses Palmér <moses@blockmastersecurity.com>
2014-05-07 10:38:21 +0200

    Added package information file

--- lib/truepy/__init__.py
+++ lib/truepy/__init__.py
@@ -15,3 +15,5 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 '''
+
+from ._info import *
