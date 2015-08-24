916e3b361d0c77a054abc7460a12cba1a7d1e33d
Moses Palmér <mosespalmer@gmail.com>
2015-08-24 21:03:02 +0200

    Correctly set path for tests

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@
 import os
 import setuptools
 import sys
-sys.path.append('.')
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
 
 # The directories in which the packages can be found
