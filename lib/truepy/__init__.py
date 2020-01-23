651ee85ff8ac192161e52f9f7e365bc1796062f4
Moses Palmér <mosespalmer@gmail.com>
2020-01-23 18:03:39 +0100

    Updated copyright years

--- lib/truepy/__init__.py
+++ lib/truepy/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # truepy
-# Copyright (C) 2014-2015 Moses Palmér
+# Copyright (C) 2014-2020 Moses Palmér
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
